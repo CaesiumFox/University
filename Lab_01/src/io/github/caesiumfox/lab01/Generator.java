1dae14ff4263e0ff8ee22f1fc74047406a37acb6
CaesiumFox <avdeev20020904@gmail.com>
2020-09-08 20:54:40 +0300

    Corrected the variant

--- Lab_01/src/io/github/caesiumfox/lab01/Generator.java
+++ Lab_01/src/io/github/caesiumfox/lab01/Generator.java
@@ -1,68 +1,42 @@
 package io.github.caesiumfox.lab01;
 
 import java.lang.Math;
-import java.util.Random;
-import java.util.Date;
 
 public class Generator {
-	private long seed;
-	private Random rand;
-
-	public Generator() {
-		this.seed = new Date().getTime();
-		rand = new Random(this.seed);
-	}
-
-	public Generator(long seed) {
-		this.seed = seed;
-		rand = new Random(this.seed);
-	}
-
-	public long GetSeed() {
-		return seed;
-	}
-
-	public int[] GenerateArithmetic(int from, int step, int count) {
-		int[] result = new int[count];
+	public static short[] GenerateArithmetic(short from, short step, int count) {
+		short[] result = new short[count];
 		result[0] = from;
 		for(int i = 1; i < count; i++) {
-			result[i] = result[i - 1] + step;
+			result[i] = (short)(result[i - 1] + step);
 		}
 		return result;
 	}
 
-	public double[] GenerateRandom(double minval, double maxval, int count) {
+	public static double[] GenerateRandom(double minval, double maxval, int count) {
 		double[] result = new double[count];
 		for(int i = 0; i < count; i++) {
-			result[i] = rand.nextDouble() * (maxval - minval) + minval;
+			result[i] = Math.random() * (maxval - minval) + minval;
 		}
 		return result;
 	}
 
-	public double[][] GenerateGArray(int[] d, double[] x) {
+	public static double[][] GenerateGArray(short[] d, double[] x) {
 		double[][] g = new double[d.length][x.length];
 		for(int i = 0; i < d.length; i++) {
 			for(int j = 0; j < x.length; j++) {
 				switch(d[i]) {
-				case 19:
+				case 10:
 					g[i][j] = Math.pow(
-						Math.atan(0.2 * (x[j] + 1.0) / 14.0),
-						0.25 / (Math.asin(Math.exp(-Math.abs(x[j]))) - 1.0)
+						Math.atan(Math.pow(((x[j] + 5.) / 2.) * Math.E + 1., 2.)),
+						Math.pow((Math.pow(x[j]/(x[j]-.5), 2.) - 1.) / Math.cos(x[j]), 3.)
 					);
-					break;
-				case 5:  // Is there anything in Java similar
-				case 9:  // to C++'s [[fallthrough]] to show
-				case 11: // that omitting 'break' statements
-				case 13: // in swith-case structures is not
-				case 17: // a mistake?
-				case 23:
-					g[i][j] = Math.cos(Math.cos(Math.cbrt(x[j])));
-					break;
+				case 4:
+				case 8:
+				case 14:
+				case 18:
+					g[i][j] = Math.log(Math.exp(Math.asin(1. / Math.exp(Math.abs(x[j])))));
 				default:
-					g[i][j] = Math.pow(
-						0.25 - Math.pow(Math.cbrt(Math.pow(x[j], x[j] - 3.0 / 4.0)), 3.0 + Math.cos(Math.cbrt(x[j]))),
-						Math.asin(Math.exp(Math.cbrt(-Math.pow(4.0 / Math.abs(x[j]), x[j]))))
-					);
+					g[i][j] = 3. / (Math.atan(((x[j] + 5.) / 2.) * Math.E + 1.) + 3.);
 					break;
 				}
 			}
--- Lab_01/src/io/github/caesiumfox/lab01/Main.java
+++ Lab_01/src/io/github/caesiumfox/lab01/Main.java
@@ -1,39 +1,20 @@
 package io.github.caesiumfox.lab01;
 
+import java.util.Locale;
+
 public class Main {
-	private static int[] d;
+	private static short[] d;
 	private static double[] x;
 	private static double[][] g;
-	private static Generator gen;
-
-	private static boolean seedSet = false;
-	private static long seed = 0;
 
 	public static void main(String[] args) {
-
-		for(int i = 0; i < args.length; i++) {
-			if(args[i].matches("-?\\d")) {
-				seedSet = true;
-				seed = Integer.parseInt(args[0]);
-			}
-			else {
-				System.out.printf("Invalid option: %s", args[i]);
-				System.exit(1);
-			}
-		}
-
-		if(seedSet)
-			gen = new Generator(seed);
-		else
-			gen = new Generator();
-
-		d = gen.GenerateArithmetic(3, 2, 12);
-		x = gen.GenerateRandom(-6.0, 8.0, 15);
-		g = gen.GenerateGArray(d, x);
+		d = Generator.GenerateArithmetic((short)4, (short)2, 8);
+		x = Generator.GenerateRandom(-5.0, 15.0, 17);
+		g = Generator.GenerateGArray(d, x);
 
 		for(int i = 0; i < d.length; i++) {
 			for(int j = 0; j < x.length; j++) {
-				System.out.printf("g[%d][%d] = %.4f\n", i, j, g[i][j]);
+				System.out.printf(Locale.US, "g[%d][%d] = %.2f\n", i, j, g[i][j]);
 			}
 		}
 	}
