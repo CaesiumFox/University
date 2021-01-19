9c2ecdd9d5959fa4e26b7d9faf052bc9ba501a18
CaesiumFox <avdeev20020904@gmail.com>
2021-01-19 14:39:58 +0300

    Small Update

--- Programming/Lab_04/src/main/java/io/github/caesiumfox/lab04/environment/Way.java
+++ Programming/Lab_04/src/main/java/io/github/caesiumfox/lab04/environment/Way.java
@@ -3,7 +3,7 @@ package io.github.caesiumfox.lab04.environment;
 import io.github.caesiumfox.lab04.PhysicalObject;
 import io.github.caesiumfox.lab04.exceptions.EmptyNameException;
 
-public class Way extends PhysicalObject {
+public abstract class Way extends PhysicalObject {
     private int width;
 
     public Way(String name, int width) throws EmptyNameException {
