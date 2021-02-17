2aeb533e480f65b0026069f8e1c3de9328fc8569
CaesiumFox <avdeev20020904@gmail.com>
2021-02-17 21:52:56 +0300

    Added CommandShell

---
+++ Programming/Lab_05/src/main/java/io/github/caesiumfox/lab05/CommandShell.java
@@ -0,0 +1,15 @@
+package io.github.caesiumfox.lab05;
+
+public class CommandShell {
+    private Database database;
+
+    public CommandShell(Database database) {
+        this.database = database;
+    }
+
+    public Database getDatabase() {
+        return database;
+    }
+
+
+}
---
+++ Programming/Lab_05/src/main/java/io/github/caesiumfox/lab05/command/Command.java
@@ -0,0 +1,5 @@
+package io.github.caesiumfox.lab05.command;
+
+public abstract class Command {
+
+}
