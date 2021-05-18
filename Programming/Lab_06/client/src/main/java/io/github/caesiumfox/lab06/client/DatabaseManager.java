c535fecb031d0bca48bc48879ff3184688d157cb
CaesiumFox <avdeev20020904@gmail.com>
2021-05-18 11:24:23 +0300

    client DBM update DB info + info

--- Programming/Lab_06/client/src/main/java/io/github/caesiumfox/lab06/client/DatabaseManager.java
+++ Programming/Lab_06/client/src/main/java/io/github/caesiumfox/lab06/client/DatabaseManager.java
@@ -3,13 +3,13 @@ package io.github.caesiumfox.lab06.client;
 import io.github.caesiumfox.lab06.common.Database;
 import io.github.caesiumfox.lab06.common.KeyWord;
 import io.github.caesiumfox.lab06.common.MutableDatabaseInfo;
-import io.github.caesiumfox.lab06.common.entry.Movie;
-import io.github.caesiumfox.lab06.common.entry.MpaaRating;
+import io.github.caesiumfox.lab06.common.entry.*;
 import io.github.caesiumfox.lab06.common.exceptions.*;
 import sun.nio.ch.Net;
 
 import java.io.IOException;
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Set;
 
@@ -17,6 +17,18 @@ public class DatabaseManager implements Database {
     MutableDatabaseInfo databaseInfo;
 
     public DatabaseManager() throws IOException {
+        NetworkManager.byteBuffer.clear();
+        NetworkManager.byteBuffer.put(KeyWord.getCode(KeyWord.GET_INFO));
+        NetworkManager.byteBuffer.flip();
+
+        NetworkManager.send();
+        NetworkManager.receive();
+
+        NetworkManager.byteBuffer.get(); // for SOME_LEFT
+        databaseInfo.getFromByteBuffer(NetworkManager.byteBuffer);
+    }
+
+    public void updateDatabaseInfo() throws IOException {
         NetworkManager.byteBuffer.clear();
         NetworkManager.byteBuffer.put(KeyWord.getCode(KeyWord.GET_INFO));
         NetworkManager.byteBuffer.flip();
@@ -84,7 +96,32 @@ public class DatabaseManager implements Database {
 
     @Override
     public void info(PrintStream output) {
+        output.println("--- Database info ---");
+        output.print("  Input File:      ");
+        output.println(databaseInfo.getInputFile().length() == 0 ? "<N/A>" : databaseInfo.getInputFile());
+        output.print("  Creation Date:   ");
+        output.println(new SimpleDateFormat(Client.dateFormat).format(databaseInfo.getCreationDate()));
+        output.print("  Max ID:          ");
+        output.println(databaseInfo.getMaxID());
+        output.print("  N/O Elements:    ");
+        output.println(databaseInfo.getNumberOfElements());
 
+        output.println("  Collection type: LinkedHashMap");
+        output.println("  Fields:");
+        output.println("    ID: [1 - 2147483647]");
+        output.println("    Name: Not empty string");
+        output.println("    Coordinates:");
+        output.format("      X: [%f, %f]\n", Coordinates.minX, Coordinates.maxX);
+        output.format("      Y: [%f, %f]\n", Coordinates.minY, Coordinates.maxY);
+        output.format("    Creation Date: %s\n", Client.dateFormat);
+        output.println("    Oscars Count: [1 - 9223372036854775807]");
+        output.format("    Genre: %s\n", MovieGenre.listConstants());
+        output.format("    MPAA Rating: %s\n", MpaaRating.listConstants());
+        output.println("    Director (may be null):");
+        output.println("      Name: Not empty string");
+        output.format("      Passport ID (may be null): string with %d to %d characters\n",
+                Person.passportIDMinLen, Person.passportIDMaxLen);
+        output.format("      Hair Color: %s\n", Color.listConstants());
     }
 
     @Override
