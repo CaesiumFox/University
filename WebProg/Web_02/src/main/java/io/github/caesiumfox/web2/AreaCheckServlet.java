baf590296c180adda29d77a70f0e684c5d91a95b
CaesiumFox <avdeev20020904@gmail.com>
2021-11-17 18:46:54 +0300

    Added empty Web_02

---
+++ WebProg/Web_02/src/main/java/io/github/caesiumfox/web2/AreaCheckServlet.java
@@ -0,0 +1,12 @@
+package io.github.caesiumfox.web2;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+public class AreaCheckServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+}
---
+++ WebProg/Web_02/src/main/java/io/github/caesiumfox/web2/ControllerServlet.java
@@ -0,0 +1,14 @@
+package io.github.caesiumfox.web2;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+public class ControllerServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+}
