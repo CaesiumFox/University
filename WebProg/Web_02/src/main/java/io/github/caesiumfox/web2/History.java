51445fbfbd1a6e2276481c4f2a135f2d18e75994
CaesiumFox <avdeev20020904@gmail.com>
2021-11-30 23:31:13 +0300

    Shartened code & added toHtml()

--- WebProg/Web_02/src/main/java/io/github/caesiumfox/web2/History.java
+++ WebProg/Web_02/src/main/java/io/github/caesiumfox/web2/History.java
@@ -1,25 +1,21 @@
 package io.github.caesiumfox.web2;
 
-import java.io.Serializable;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-
-public class History extends ArrayList<History.Entry> {
-    public static class Entry implements Serializable {
-        public double x;
-        public double y;
-        public double r;
+public class History extends java.util.ArrayList<History.Entry> {
+    public static class Entry implements java.io.Serializable {
+        public double x, y, r;
         public boolean hit;
-        public LocalDateTime script_start;
-        public Duration script_duration;
-    }
-
-    public History(int capacity) {
-        super(capacity);
-    }
-
-    public History() {
-        super();
+        public java.time.LocalDateTime script_start;
+        public java.time.Duration script_duration;
+        public String toHtml() {
+            return "<tr><td>" + String.valueOf(x) +
+                    "</td><td>" + String.valueOf(y) +
+                    "</td><td>" + String.valueOf(y) +
+                    "</td><td>" + (hit ? "Да" : "Нет") +
+                    "</td><td>" + script_start.format(new java.time.format.DateTimeFormatterBuilder()
+                            .appendPattern("dd.MM.yyyy HH:mm:ss")
+                            .toFormatter()) +
+                    "</td><td>" + String.valueOf(script_duration.toNanos()) +
+                    "</td></tr>";
+        }
     }
 }
