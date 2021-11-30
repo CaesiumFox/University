03b02650e6bd37ce7234b57ed7b40fc0a6c9144c
CaesiumFox <avdeev20020904@gmail.com>
2021-11-30 23:30:42 +0300

    Fixed paths

--- WebProg/Web_02/src/main/java/io/github/caesiumfox/web2/ControllerServlet.java
+++ WebProg/Web_02/src/main/java/io/github/caesiumfox/web2/ControllerServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.annotation.*;
 public class ControllerServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("/").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 
     @Override
