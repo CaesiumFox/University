92a00b669d3961e6f2c73b2b654ea7390589d312
CaesiumFox <avdeev20020904@gmail.com>
2022-01-24 12:14:17 +0300

    Spring Init

---
+++ WebProg/Web_04/src/main/java/io/github/caesiumfox/web4/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.github.caesiumfox.web4;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Web04Application.class);
+	}
+
+}
---
+++ WebProg/Web_04/src/test/java/io/github/caesiumfox/web4/Web04ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.caesiumfox.web4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
