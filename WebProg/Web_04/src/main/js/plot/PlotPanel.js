ae16170a7af1e22797b26c29f1c0802995b31074
Caesium Fox <avdeev20020904@gmail.com>
2022-02-07 14:32:30 +0300

    Added ajax to plot

--- WebProg/Web_04/src/main/js/plot/PlotPanel.js
+++ WebProg/Web_04/src/main/js/plot/PlotPanel.js
@@ -1,18 +1,48 @@
 import Curve from './Curve.js';
 import Axes from './Axes.js';
 import Points from './Points.js';
+import $ from 'jquery';
 
 export default function PlotPanel(props) {
-    <svg id="the_image"
-            width="100%"
-            height="100%"
-            viewBox="0 0 100 100">
-        <style>
-            @import url("/svgstyle.css");
-        </style>
+    let dispatch = useDispatch();
+    let r = useSelector(state => state.imgR);
 
-        <Curve r={this.props.r}/>
-        <Axes r={this.props.r}/>
-        <Points/>
-    </svg>
+    function sendImgRequest(e) {
+        e.preventDefault();
+        let imgObj = $("#the_image");
+        let mx = e.offsetX;
+        let my = e.offsetY;
+        let iw = imgObj.width();
+        let ih = imgObj.height();
+        $.post({
+            url: "/post-data",
+            dataType: "json",
+            data: {
+                type: "plot",
+                x: ((10 * mx / iw - 5) * r / 4),
+                y: ((5 - 10 * my / ih) * r / 4),
+                r: r
+            }
+        }).done(function(data) {
+            dispatch({type: "PUSH_ENTRIES", payload: data.entries});
+        }).fail(function(jqXHR, textStatus) {
+            alert('Sending ajax failed: ' + textStatus);
+        });
+    }
+
+    return (
+        <svg id="the_image"
+                width="100%"
+                height="100%"
+                viewBox="0 0 100 100"
+                onclick={sendImgRequest}>
+            <style>
+                @import url("/svgstyle.css");
+            </style>
+
+            <Curve r={this.props.r}/>
+            <Axes r={this.props.r}/>
+            <Points/>
+        </svg>
+    );
 }
